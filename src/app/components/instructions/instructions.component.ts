110908288a8c921af4530bda960b580c2b5773fb
Sabrina Lu <sabrinalu@RM311.local>
2020-03-23 17:19:08 -0400

    added styling and content

--- src/app/components/instructions/instructions.component.ts
+++ src/app/components/instructions/instructions.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'instructions',
   templateUrl: './instructions.component.html',
-  styleUrls: ['./instructions.component.css']
+  styleUrls: ['./instructions.component.css', '../shared-styles.css']
 })
 export class InstructionsComponent implements OnInit {
 
