41495182ed78e522a7121949e7f0fbeacc9c4715
Sabrina Lu <sabrinalu@RM311.local>
2020-04-13 00:13:15 -0400

    added styling

--- src/app/components/narration/source.enum.ts
+++ src/app/components/narration/source.enum.ts
@@ -1,15 +1,15 @@
 export enum source {
-    beginning = "../../../assets/narrations/Beginning.mp3",
-    lancelot = '../../../assets/narrations/Lancelot.mp3',
-    minions = "../../../assets/narrations/Evil1.mp3",
-    mordred = "../../../assets/narrations/Evil2.mp3",
-    oberon = "../../../assets/narrations/Evil3.mp3",
-    allEvil = "../../../assets/narrations/Evil4.mp3",
-    closeEyes = "../../../assets/narrations/CloseEyes.mp3",
-    merlin = "../../../assets/narrations/Merlin.mp3",
-    thumbsDown = "../../../assets/narrations/ThumbsDown.mp3",
-    percival = "../../../assets/narrations/Percival.mp3",
-    ending = "../../../assets/narrations/End.mp3"
+    beginning = "../../website/assets/narrations/Beginning.mp3",
+    lancelot = '../../website/assets/narrations/Lancelot.mp3',
+    minions = "../../website/assets/narrations/Evil1.mp3",
+    mordred = "../../website/assets/narrations/Evil2.mp3",
+    oberon = "../../website/assets/narrations/Evil3.mp3",
+    allEvil = "../../website/assets/narrations/Evil4.mp3",
+    closeEyes = "../../website/assets/narrations/CloseEyes.mp3",
+    merlin = "../../website/assets/narrations/Merlin.mp3",
+    thumbsDown = "../../website/assets/narrations/ThumbsDown.mp3",
+    percival = "../../website/assets/narrations/Percival.mp3",
+    ending = "../../website/assets/narrations/End.mp3"
 
 
 }
